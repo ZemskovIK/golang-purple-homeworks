be27e7e68e9d5029b152bed704d17b7a9fb4effe
Igor Zemskov <igor@zemskoff.su>
2026-03-09 16:05:20 +0300

    feat: add user input function and empty convert function

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+func readAmount() float64 {
+	var amount float64
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&amount)
+	return amount
+}
+
+func convert(amount float64, fromCurrency string, toCurrency string) {
+	// pass
+}
+
 func main() {
 	const USDToEUR = 0.86
 	const USDToRUB = 79.15
